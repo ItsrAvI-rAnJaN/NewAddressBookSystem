51419c3b3e0180bf19202aa2b9c700b29ad74294
ravi04ez <ezekie.em@gmail.com>
2022-07-22 03:03:30 +0530

    Added Create Contacts

---
+++ AddressBookSystems/AddressBookSystem/AddressBooks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    // Creating Class 
+    public class AddressBooks
+    {
+        public string First_Name;
+        public string Last_Name;
+        public string Address;
+        public string City;
+        public string State;                                   // Taking Global Variables to Store Details
+        public int Zip_Code;
+        public long Ph_Number;
+        public string Email;
+
+        // Creating Constructor
+        public AddressBooks(string first_Name, string last_Name, string address, string city, string state, int zip_Code, long ph_Number, string email)
+        {
+            this.First_Name = first_Name;
+            this.Last_Name = last_Name;
+            this.Address = address;
+            this.City = city;
+            this.State = state;                               // Set Class Variables 
+            this.Zip_Code = zip_Code;
+            this.Ph_Number = ph_Number;
+            this.Email = email;
+        }
+
+        public void Displaydetails()
+        {
+            Console.WriteLine("\n Contact Details:- ");
+            Console.WriteLine(" Name: {0} {1} ",First_Name, Last_Name);
+            Console.WriteLine(" Address: {0}, {1}, {2} ",Address,City,State);
+            Console.WriteLine(" Zip Code: {0}",Zip_Code);
+            Console.WriteLine(" Ph. Number: {0}",Ph_Number);
+            Console.WriteLine(" Email Address: {0}",Email);
+        }
+
+    }
+}
--- AddressBookSystems/AddressBookSystem/Program.cs
+++ AddressBookSystems/AddressBookSystem/Program.cs
@@ -9,6 +9,11 @@ namespace AddressBookSystem
             Console.WriteLine("\t\t\t\t################################################");
             Console.WriteLine("\t\t\t\t\tWELCOME TO ADDRESS BOOK SYSTEM");
             Console.WriteLine("\t\t\t\t################################################");
+
+            // Creating Instance obj..
+            AddressBooks objDetails = new AddressBooks("Ravi","Ranjan","Dlf Phase 4","Gurugram","Harayana",122002,9319964621,"Ezekie.ez@gmail.com");
+            // Calling method
+            objDetails.Displaydetails();
             Console.ReadKey();
         }
     }
